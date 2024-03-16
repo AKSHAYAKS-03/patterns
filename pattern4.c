3a516c9ee2287a19d9811506e10a97d1201d1236
AKSHAYAKS-03 <121333474+AKSHAYAKS-03@users.noreply.github.com>
2024-03-16 21:52:31 +0530

    Create pattern4.c

---
+++ pattern4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int n = 3; // Number of rows
+       for (int i = 0; i < n; i++) {
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            printf("  ");
+        }
+
+        // Print increasing numbers
+        for (int j = 0; j < 2 * i + 1; j++) {
+            printf("* ");
+        }
+
+        // Print spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            printf("  ");
+        }
+
+        printf("\n");
+    }
+
+    for (int i = 1; i < n; i++) {
+        // Print spaces
+        for (int j = 0; j < i; j++) {
+            printf("  ");
+        }
+
+        // Print increasing numbers
+        for (int j = 0; j < 2*n - (2 * i + 1); j++) {
+            printf("* ");
+        }
+
+        // Print spaces
+        for (int j = 0; j < i ; j++) {
+            printf("  ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+    *     
+  * * *   
+* * * * * 
+  * * *   
+    *     
+
+
