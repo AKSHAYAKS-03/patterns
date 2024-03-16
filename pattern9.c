62350e07cfa49f6bc8636f9e4f182755224fd50b
AKSHAYAKS-03 <121333474+AKSHAYAKS-03@users.noreply.github.com>
2024-03-16 22:41:33 +0530

    Create pattern9.c

---
+++ pattern9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+        int n =4 ;
+         for(int i=0;i<n;i++)
+        {
+             for(char c ='A' ; c<= 'A' + i ;c++){
+                
+            printf("%c ",c);
+        }
+      printf("\n");  
+    }
+    
+
+}
+
+A 
+A B 
+A B C 
+A B C D 
