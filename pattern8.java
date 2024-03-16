fc54ecfdc6f410b9fd0722ea49c8c0c6edc4dab3
AKSHAYAKS-03 <121333474+AKSHAYAKS-03@users.noreply.github.com>
2024-03-16 22:29:47 +0530

    Create pattern8.java

---
+++ pattern8.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static void main(String a[]) {
+        // Write your code here
+      int n=4;
+        int k =1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++){
+            System.out.print(k+" ");
+            k++;;
+        }
+      System.out.println();  
+    }
+}
+}
+
+
+1
+2 3
+4 5 6 
+7 8 9 10
